66bccc8e4b734246fc3988ceb59302978559582a
Vivaan Atharva <vivaanjatharva@gmail.com>
2026-01-07 22:08:55 +0530

    Practice: reversing array using pointers

---
+++ reverse_array_pointers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[100];
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int *start = arr;
+    int *end = arr + n - 1;
+
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << "Reversed array:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
