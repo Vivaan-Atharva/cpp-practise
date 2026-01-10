9c70c64dead1b1ab0da81609c16c5cb164fbf929
Vivaan Atharva <vivaanjatharva@gmail.com>
2026-01-10 22:27:58 +0530

    Practice: recursive binary search

---
+++ binary_search_recursive.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int binarySearchRecursive(int arr[], int low, int high, int key) {
+    if (low > high)
+        return -1;
+
+    int mid = low + (high - low) / 2;
+
+    if (arr[mid] == key)
+        return mid;
+    else if (arr[mid] < key)
+        return binarySearchRecursive(arr, mid + 1, high, key);
+    else
+        return binarySearchRecursive(arr, low, mid - 1, key);
+}
+
+int main() {
+    int n, key;
+    int arr[100];
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements in sorted order:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int index = binarySearchRecursive(arr, 0, n - 1, key);
+
+    if (index != -1)
+        cout << "Element found at index " << index << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}
